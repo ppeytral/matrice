8db6e99330473ca3eaef380cffb1ce999cd9df59
ppeytral <p.peytral@gmail.com>
2023-04-02 07:43:27 +0200

    correction on anonymizeInput.js

--- anonymizeInput.js
+++ anonymizeInput.js
@@ -1,16 +1,29 @@
+function fnCreateMask(sInput) {
+	let iLength = sInput.length;
+	let sMask = "";
+
+	for (let i = 0; i < iLength - 4; i++) {
+		sMask += "*";
+	}
+
+	return sMask;
+}
+
 function anonymizeInput(sInput) {
 	let sResult = sInput;
 
 	if (sInput.length > 4) {
-		let sMask = "****";
-		sResult = sMask + sInput.slice(4);
+		sResult = fnCreateMask(sInput) + sInput.slice(sInput.length - 4);
 		}
 	console.log(sResult);
 }
 
 module.exports = anonymizeInput;
+module.exports = fnCreateMask;
 
 /*
 anonymizeInput("Patrice");
 anonymizeInput("Test");
+anonymizeInput("he sees ghosts");
+anonymizeInput("roses are red");
 */
